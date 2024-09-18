32b50f1ffe9f2193848721c7237cadbd8bc1b772
isaacdandrea <isaacdandrea@protonmail.com>
2024-09-18 11:58:58 -0300

    feat(t1): obtem previsao a partir de coords

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const dotenv = require('dotenv');
 const readline = require('readline');
 dotenv.config();
-const { PROTOCOL, BASE_URL, APP_ID, Q, LIMIT} = process.env;
+const { PROTOCOL, BASE_URL, APP_ID, LIMIT, WEATHER_BASE_URL} = process.env;
 
 const getCityCoordinates = (cityName) => {
     const url = `${PROTOCOL}://${BASE_URL}?q=${cityName}&limit=${LIMIT}&appid=${APP_ID}`;
@@ -19,17 +19,43 @@ const getCityCoordinates = (cityName) => {
         })
         .catch(error => {
             console.log(`Erro ao buscar a cidade: ${error.message}`);
+            return null;
         });
 };
 
+const climaPorCoordenada = (lat, lon) => {
+    const url = `${PROTOCOL}://${WEATHER_BASE_URL}?lat=${lat}&lon=${lon}&appid=${APP_ID}&units=metric&lang=pt_br`;
+
+    return axios.get(url)
+        .then(response => {
+            const { feels_like } = response.data.main;
+            const { description } = response.data.weather[0];
+            console.log(`Sensação térmica: ${feels_like}°C`);
+            console.log(`Descrição do clima: ${description}`);
+            return { feels_like, description };
+        })
+        .catch(error => {
+            console.log(`Erro ao buscar o clima: ${error.message}`);
+        });
+};
+
+// Função para perguntar a cidade ao usuário e obter as informações de clima
 const askCity = () => {
     const rl = readline.createInterface({
         input: process.stdin,
         output: process.stdout
     });
 
-    rl.question('Digite o nome da cidade que deseja buscar: ', (nomeCidade) => {
-        getCityCoordinates(nomeCidade).finally(() => rl.close());
+    rl.question('Digite o nome da cidade para buscar: ', (nomeCidade) => {
+        console.log('Buscando coordenadas...');
+        getCityCoordinates(nomeCidade)
+            .then(coordinates => {
+                if (coordinates) {
+                    console.log('Buscando informações de clima...');
+                    climaPorCoordenada(coordinates.lat, coordinates.lon);
+                }
+            })
+            .finally(() => rl.close());
     });
 };
 
--- readme.md
+++ readme.md
@@ -1,15 +1,15 @@
 
-# Trabalho de Arquitetura e Soluçoes Computacionais
-Aplicativo que realiza consultas direcionadas a um
-serviço de condições climáticas
+## Instalação
 
----
+Após fazer git clone, instale as dependencias usando
+```bash
+  npm install
+```
+Renomeie o arquivo ```.env.example``` para apenas ```.env```
 
-## Integrantes:
-- Gabriel Carrasco - 22.00906-0
-- Gustavo Yudji Hiromoto - 22.00849-0
-- Isaac d'Andrea - 22.01841-7
-- Lucas Pennone -  21.01086-2
-- Matheus Alatxeve - 20.00528-8
-- Othavio Carvalho Losovoi - 22.01545-0
-
+No arquivo ```.env``` atualize o campo
+```bash 
+APP_ID= <SuaChave>
+```
+e substitua <SuaChave> por uma chave de API gerada no site
+[https://openweathermap.org/api](https://openweathermap.org/api)
